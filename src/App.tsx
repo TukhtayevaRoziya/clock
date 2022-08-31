037568c310566be55cc8b5671c9b51197987446e
TukhtayevaRoziya <muhammadumarmuhammadzoda@gmail.com>
2022-08-31 14:18:22 +0500

    connect api

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
-import axios from "axios";
-import React, { useEffect, useRef } from "react";
+import React, { useRef } from "react";
 import { Stage, Layer, Rect, Group } from "react-konva";
 import { Html } from "react-konva-utils";
 
@@ -10,67 +9,47 @@ function App() {
   const transformerRef = useRef(text as any);
   const stageRef = useRef();
 
-  var config = {
-    method: 'get',
-    url: 'https://www.timeapi.io/api/Time/current/zone?timeZone=Asia/Tashkent',
-    headers: { 
-      "Content-Type": "application/json",
-      Accept: "application/json",
-      accept: 'application/json',
-      'Access-Control-Allow-Origin':"*"
-    }
-  };
-
-  useEffect(() => {
-    axios(config)
-    .then(function (response) {
-      console.log(JSON.stringify(response.data));
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
-    
-    
-  }, []);
   return (
     <div>
-      <Stage
-        // @ts-ignore
-        ref={stageRef}
-        width={window.innerWidth}
-        height={window.innerHeight}
-      >
-        <Layer>
-          <Group groupName="container">
-            <Group
-              hash="test"
-              draggable
-              scaleX={1}
-              scaleY={1}
-              name="clock"
-              x={120}
-              y={100}
-              type="widget"
-              onClick={(event) => {
-                transformerRef.current.nodes([event.target.getParent()]);
-              }}
-            >
-              <Html
-                divProps={{
-                  style: {
-                    pointerEvents: "none",
-                    width: "300px",
-                    height: "300px",
-                  },
+      <h1 style={{ textAlign: "center" }}>America Mexico City Time</h1>
+
+        <Stage
+          // @ts-ignore
+          ref={stageRef}
+          width={window.innerWidth}
+          height={window.innerHeight}
+        >
+          <Layer>
+            <Group groupName="container">
+              <Group
+                hash="test"
+                draggable
+                scaleX={1}
+                scaleY={1}
+                name="clock"
+                x={120}
+                y={100}
+                type="widget"
+                onClick={(event) => {
+                  transformerRef.current.nodes([event.target.getParent()]);
                 }}
               >
-                <Clock />
-              </Html>
-              <Rect width={300} height={300} />
+                <Html
+                  divProps={{
+                    style: {
+                      pointerEvents: "none",
+                      width: "300px",
+                      height: "300px",
+                    },
+                  }}
+                >
+                  <Clock />
+                </Html>
+                <Rect width={300} height={300} />
+              </Group>
             </Group>
-          </Group>
-        </Layer>
-      </Stage>
+          </Layer>
+        </Stage>
     </div>
   );
 }
--- src/Clock.tsx
+++ src/Clock.tsx
@@ -1,61 +1,62 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 import "./index.css";
+import axios from "axios";
 
-export default class ClockComponent extends Component {
-  timerId: NodeJS.Timer | undefined;
-  state = {
-    time: new Date(),
-  };
+const Clock = () => {
+  const [time, setTime] = useState([]);
+  const [hour, setHour] = useState(0);
+  const [min, setMin] = useState(0);
+  const [sec, setSec] = useState(0);
 
-  componentDidMount() {
-    console.log(this.state.time.getMinutes());
-    console.log(this.state.time);
-
-    this.timerId = setInterval(() => {
-      this.setState({
-        time: new Date(),
+  useEffect(() => {
+    axios.get('http://worldtimeapi.org/api/timezone/America/Mexico_City')
+      .then(function (response) {
+        setTime(response.data.datetime);
+        console.log(response.data.datetime);
+        
+        setHour(time && time[11] + time[12]);
+        setMin(time && time[14] + time[15]);
+        setSec(time && time[17] + time[18]);
+      })
+      .catch(function (error) {
+        console.log(error);
       });
-    }, 1000);
-  }
+  }, [sec, time]);
 
-  componentWillMount() {
-    clearInterval(this.timerId);
-  }
+  return (
+    <div className="clock">
+      <div
+        className="hour_hand"
+        style={{
+          transform: `rotateZ(${hour * 30}deg)`,
+        }}
+      />
+      <div
+        className="min_hand"
+        style={{
+          transform: `rotateZ(${min * 6}deg)`,
+        }}
+      />
+      <div
+        className="sec_hand"
+        style={{
+          transform: `rotateZ(${sec * 6}deg)`,
+        }}
+      />
+      <span className="twelve">12</span>
+      <span className="one">1</span>
+      <span className="two">2</span>
+      <span className="three">3</span>
+      <span className="four">4</span>
+      <span className="five">5</span>
+      <span className="six">6</span>
+      <span className="seven">7</span>
+      <span className="eight">8</span>
+      <span className="nine">9</span>
+      <span className="ten">10</span>
+      <span className="eleven">11</span>
+    </div>
+  );
+};
 
-  render() {
-    return (
-      <div className="clock">
-        <div
-          className="hour_hand"
-          style={{
-            transform: `rotateZ(${this.state.time.getHours() * 30}deg)`,
-          }}
-        />
-        <div
-          className="min_hand"
-          style={{
-            transform: `rotateZ(${this.state.time.getMinutes() * 6}deg)`,
-          }}
-        />
-        <div
-          className="sec_hand"
-          style={{
-            transform: `rotateZ(${this.state.time.getSeconds() * 6}deg)`,
-          }}
-        />
-        <span className="twelve">12</span>
-        <span className="one">1</span>
-        <span className="two">2</span>
-        <span className="three">3</span>
-        <span className="four">4</span>
-        <span className="five">5</span>
-        <span className="six">6</span>
-        <span className="seven">7</span>
-        <span className="eight">8</span>
-        <span className="nine">9</span>
-        <span className="ten">10</span>
-        <span className="eleven">11</span>
-      </div>
-    );
-  }
-}
+export default Clock;
